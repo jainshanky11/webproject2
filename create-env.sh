7f25523e0d34633952823aa659e1481b95843089
Shashank <jainshanky11@gmail.com>
2018-10-20 21:08:21 +0530

    script file modified

--- create-env.sh
+++ create-env.sh
@@ -5,9 +5,9 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating webproject1 $i container.."
+    echo "Creating webproject$i container.."
     sleep 1
     docker run --name webproject$i -d -it jainshanky11/webproject2-img /bin/bash
-    echo "webproject1 $i container has been created!"
+    echo "webproject$i container has been created!"
 	echo "=============================="
 done
